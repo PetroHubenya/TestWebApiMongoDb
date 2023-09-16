89b7dfa8412d81c4b7ba6272970fa6657320f6c5
Petro Hubenya <p.hubenya@gmail.com>
2023-09-15 23:40:28 -0400

    MongoDB settings added to the appsettings.json. MongoDbSettings class done. MongoDBService done (to be updated).

---
+++ DataAccessLayer/MongoDBService.cs
@@ -0,0 +1,49 @@
+﻿using Interfaces;
+using Microsoft.Extensions.Options;
+using Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class MongoDBService : IDataService
+    {
+        private readonly IMongoCollection<CurveData> _curveData;
+
+        public MongoDBService(IOptions<MongoDbSettings> mongoDBSettings)
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _curveData = database.GetCollection<CurveData>(mongoDBSettings.Value.CollectionName);
+        }
+
+        public void CreateCurveData(CurveData curveData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCurveData(string curveName, int curveDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CurveData> GetAllCurves()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CurveData GetCurveData(string curveName, int curveDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CurveData UpdateCurveData(CurveData newData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
