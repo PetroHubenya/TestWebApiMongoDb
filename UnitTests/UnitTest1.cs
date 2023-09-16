72e252381aac0296e62a8510b26e4971a4295f26
Petro Hubenya <p.hubenya@gmail.com>
2023-09-15 20:05:04 -0400

    Add project files.

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+            Assert.Pass();
+        }
+    }
+}
