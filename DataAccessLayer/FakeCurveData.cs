80c92605978a29d762a2e68824293efcf9750c5c
Petro Hubenya <p.hubenya@gmail.com>
2023-09-15 20:26:24 -0400

    FakeCurveData done.

---
+++ DataAccessLayer/FakeCurveData.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+using System.Security.Cryptography;
+
+namespace DataAccessLayer
+{
+    public class FakeCurveData
+    {
+        private CurveData CurveObject = new CurveData()
+        {
+            _id = "idString",
+            CurveName = "CurveName",
+            Currency = "CAD",
+            CurvePoints = new List<CurvePoint>
+            {
+                new CurvePoint()
+                {
+                    Date = 20230914,
+                    Tenor = "Tenor",
+                    Value = 0.05
+                },
+                new CurvePoint()
+                {
+                    Date = 20230915,
+                    Tenor = "Tenor",
+                    Value = 0.04
+                }
+            }
+
+        };
+
+    }
+}
