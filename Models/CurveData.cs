226bd29a0bc42d45bc9c178e90f25f6f85171078
Petro Hubenya <p.hubenya@gmail.com>
2023-09-15 20:13:39 -0400

    Models created.

---
+++ Models/CurveData.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+    public class CurveData
+    {
+        public string _id { get; set; }
+
+        public string CurveName { get; set; }
+
+        public string Currency { get; set; }
+
+        public List<CurvePoint> CurvePoints { get; set; }
+
+    }
+}
---
+++ Models/CurvePoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class CurvePoint
+    {
+        public int Date { get; set; }
+
+        public string Tenor { get; set; }
+
+        public double Value { get; set; }
+    }
+}
